3f7d7b006a2fa6ae7eadaaff46ae793851883e36
naassuun <anastasia.sumina@urfu.me>
2025-04-21 20:26:25 +0500

    Update streamlit_test.py

--- streamlit_test.py
+++ streamlit_test.py
@@ -23,9 +23,23 @@ plt.legend()
 st.pyplot(plt.gcf())
 
 #создаем виджет с выпадающим списком
-options = st.multiselect('Книги какого года вас интересуют?', db['Год издания'].unique())
-st.write('Вы выбрали: ', options)
+options1 = st.multiselect('Книги какого года вас интересуют?', db['Год издания'].unique())
+st.write('Вы выбрали: ', options1)
 #создаем и выводим новую таблицу, сформированную по выбранным данным
-new_db = db[(db['Год издания'].isin(options))]
-st.write(new_db)
+new_db1 = db[(db['Год издания'].isin(options1))]
+st.write(new_db1)
+
+#создаем виджет с выпадающим списком
+options2 = st.multiselect('Книги какого автора вас интересуют?', db['Автор'].unique())
+st.write('Вы выбрали: ', options2)
+#создаем и выводим новую таблицу, сформированную по выбранным данным
+new_db2 = db[(db['Автор'].isin(options2))]
+st.write(new_db2)
+
+#создаем виджет с выпадающим списком
+options3 = st.multiselect('Книги какого издательства вас интересуют?', db['Издательство'].unique())
+st.write('Вы выбрали: ', options3)
+#создаем и выводим новую таблицу, сформированную по выбранным данным
+new_db3 = db[(db['Издательство'].isin(options3))]
+st.write(new_db3)
 
